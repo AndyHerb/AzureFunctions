001b52cf9cc584f26bf1b0f81f22dd2251ab61d3
AndyHerb <Andy_Herb@Hotmail.com>
2017-04-02 00:28:33 +0100

    Initial Add
    Receives an incoming WebHook in JSON format and creates new entity in
    Table Storage

---
+++ HttpTrigger-PowerShell-TableStorage/run.ps1
@@ -0,0 +1,10 @@
+﻿$requestBody = Get-Content $req -Raw | ConvertFrom-Json
+Write-Output "PowerShell script processed queue message '$requestBody'"
+
+$entity = [PSObject]@{
+  PartitionKey = $requestBody.room  
+  RowKey = get-date -Format "yyyy-MM-dd H:m:s.ms"
+  Temp = $requestBody.temp
+}
+
+$entity | ConvertTo-Json | Out-File -Encoding UTF8 $outputTable
